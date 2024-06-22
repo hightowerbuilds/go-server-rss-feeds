8f5cd8baf87741de896c99ac71f3b616426a8e4e
Luke Hightower <111666694+hightowerbuilds@users.noreply.github.com>
2024-06-21 17:00:17 -0700

    mods venors env dotenv librrary

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
 	fmt.Println("some tuff stuff")
+
+	godotenv.Load(".env")
+
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT no no")
+	}
+	fmt.Println("Port:", portString)
 }
